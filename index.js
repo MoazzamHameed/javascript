246ee2619c8c1b1ce8da660671a00be11436d532
Moazzam Hameed <cleveransari2704@gmail.com>
2023-04-11 01:49:17 +0500

    Assignment 2

--- index.js
+++ index.js
@@ -84,14 +84,104 @@
 
 // }
 // Question 7 :
-let rows = 5;
-for(let i = 1; i <= rows; i++ ){
-    let pattern = "";
-    for(let j = 1; j <= i; j++){
-        pattern += "*";
-    }
-    console.log(pattern);
+// let rows = 5;
+// for(let i = 1; i <= rows; i++ ){
+//     let pattern = "";
+//     for(let j = 1; j <= i; j++){
+//         pattern += "*";
+//     }
+//     console.log(pattern);
+// }
+// Assignment # 2;
+// Question 1:
+function createAdder(num){
+    return function(x){
+        return x + num;
+    };
 }
+const addFive = createAdder(5);
+console.log(addFive(10));
+console.log(addFive(20));
+
+// Question 2:
+
+function searchArray(arr, val){
+    if(arr.lenght === 0){
+        return false;
+    }else if (arr[0]===val){
+        return true;
+    }else{
+        return searchArray(arr.slice(1), val);
+    }
+}
+const arr = [1, 2, 3, 4, 5]; 
+console.log(searchArray(arr,3));
+
+// Question 3:
+
+function addParagraph(text){
+    const newParagraph = document.createElement('P');
+    newParagraph.textContent = text;
+    document.body.appendChild(new newParagraph);
+    }
+// Question 4:
+
+    function addListItem(text) {
+        const newListItem = document.createElement('li');
+        newListItem.textContent = text;
+        const unorderedList = document.querySelector('ul');
+        unorderedList.appendChild(newListItem);
+      }
+
+// Question 5:
+
+function changeBackgroundColor(element, color){
+element.style.BackgroundColor = color;
+}
+const myElement = document.getElementById('myelement');
+changeBackgroundColor(myElement, 'red');
+
+// Question 6:
+
+function saveObjectToLocalStorage(key, obj){
+    localStorage.setItem(key, JSON.stringify(obj))
+
+}
+const myObject = {name: 'john', age: 30};
+saveObjectToLocalStorage('myKey', myObject); 
+
+Question 7
+
+
+function getObjectFromLocalStorage(key) {
+    const obj = localStorage.getItem(key);
+    return obj ? JSON.parse(obj) : null;
+  }
+  const myObject = getObjectFromLocalStorage('myKey');
+
+  Question 8:
+
+  function saveObjectPropertiesToLocalStorage(obj) {
+    // Save object properties to localStorage
+    for (const key in obj) {
+      localStorage.setItem(key, JSON.stringify(obj[key]));
+    }
+  
+    // Retrieve object properties from localStorage and return a new object
+    const newObj = {};
+    for (let i = 0; i < localStorage.length; i++) {
+      const key = localStorage.key(i);
+      newObj[key] = JSON.parse(localStorage.getItem(key));
+    }
+    return newObj;
+  }
+   const myObject = { name: 'John', age: 30 };
+  const newObj = saveObjectPropertiesToLocalStorage(myObject);
+  console.log(newObj); // { name: 'John', age: 30 }
+   
+
+
+
 
 
 
